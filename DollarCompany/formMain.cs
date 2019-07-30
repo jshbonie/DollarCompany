0c5de2b726e0ffd7966ace02f2e23af502abb587
Joshua <jshbonie@gmail.com>
2019-07-30 11:27:36 -0400

    Add project files.

---
+++ DollarCompany/formMain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace DollarCompany
+{
+    public partial class formMain : Form
+    {
+        public formMain()
+        {
+            Thread t = new Thread(new ThreadStart(StartForm));
+            t.Start();
+            Thread.Sleep(3000);
+            InitializeComponent();
+            t.Abort();
+        }
+
+        public void StartForm()
+        {
+            Application.Run(new formSplashScreen());
+        }
+    }
+}
